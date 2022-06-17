b6da87105c197284acafa5549a61eecf8cf81a1d
archanasingh0901 <54199157+archanasingh0901@users.noreply.github.com>
2022-06-17 18:18:48 +0530

    Update weather.service.ts

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { User } from './';
+import { User } from './user.interface';
 import { Observable } from 'rxjs';
 
 @Injectable({
